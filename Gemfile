ebab0db789affd629a1f2816200080005dddb86c
Vlad <vlad.golovchak.1996@gmail.com>
2022-05-22 00:37:43 +0300

    standard login

--- Gemfile
+++ Gemfile
@@ -7,15 +7,20 @@ ruby '2.5.0'
 
 gem 'jsonapi-serializer'
 gem 'octokit'
+gem 'bcrypt'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jsom-pagination'
 gem 'jsonapi_errors_handler'
 gem 'puma', '~> 3.11'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
-gem 'sqlite3'
+
+group :production do
+  gem 'pg'
+end
 
 group :development, :test do
+  gem 'sqlite3'
   gem 'factory_bot_rails'
   gem 'pry-rails'
   gem 'rspec-rails'
--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -1,66 +1,43 @@
 # frozen_string_literal: true
 
-require 'pry'
+require 'rails_helper'
+
 describe UserAuthenticator do
-  describe '#perform' do
-    let(:authenticator) { described_class.new('sample_code') }
+  let(:user) { create :user, login: 'jsmith', password: 'secret' }
 
-    subject { authenticator.perform }
+  shared_examples_for 'authenticator' do
+    it "should create and set user's access token" do
+      expect(authenticator.authenticator).to receive(:perform).and_return(true)
+      expect(authenticator.authenticator).to receive(:user).at_least(:once).and_return(user)
+      expect { authenticator.perform }.to change{ AccessToken.count }.by(1)
+      expect(authenticator.access_token).to be_present
+    end
+  end
 
-    context 'when code is incorrect' do
-      let(:error) do
-        double('Sawyer::Resource', error: 'bad_verification_code')
-      end
+  context 'when initialized with code' do
+    let(:authenticator) { described_class.new(code: 'sample') }
+    let(:authenticator_class) { UserAuthenticator::Oauth }
 
-      before do
-        allow_any_instance_of(Octokit::Client).to receive(
-          :exchange_code_for_token
-        ).and_return(error)
-      end
-
-      it 'raise an error' do
-        expect { subject }.to raise_error(
-          UserAuthenticator::AuthenticationError
-        )
-        expect(authenticator.user).to be_nil
+    describe '#initialize' do
+      it 'should initialize proper authenticator' do
+        expect(authenticator_class).to receive(:new).with('sample')
+        authenticator
       end
     end
 
-    context 'when code is correct' do
-      let(:user_data) do
-        {
-          login: 'jsmith1',
-          url: 'http://example.com',
-          avatar_url: 'http://example.com/avatar',
-          name: 'John Smith'
-        }
-      end
+    it_behaves_like 'authenticator'
+  end
 
-      before do
-        allow_any_instance_of(Octokit::Client).to receive(
-          :exchange_code_for_token
-        ).and_return('validaccesstoken')
-
-        allow_any_instance_of(Octokit::Client).to receive(
-          :user
-        ).and_return(user_data)
-      end
-
-      it 'save the user when does not exist' do
-        expect { subject }.to change { User.count }.by(1)
-        expect(User.last.name).to eq('John Smith')
-      end
-
-      it 'reuse already registered user' do
-        user = create :user, user_data
-        expect { subject }.not_to change{ User.count }
-        expect(authenticator.user).to eq(user)
-      end
-
-      it 'create and set user\'s access token' do
-        expect { subject }.to change { AccessToken.count }.by(1)
-        expect(authenticator.access_token).to be_present
+  context 'when initialized with login & password' do
+    let(:authenticator) { described_class.new(login: 'jsmith', password: 'secret') }
+    let(:authenticator_class) { UserAuthenticator::Standard }
+    describe '#initialize' do
+      it 'should initialize proper authenticator' do
+        expect(authenticator_class).to receive(:new).with('jsmith', 'secret')
+        authenticator
       end
     end
+
+    it_behaves_like 'authenticator'
   end
 end
